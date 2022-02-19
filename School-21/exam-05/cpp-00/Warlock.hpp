dbcd75a3ac390a6b48179f8a3cef995efa5f7f2b
tlucanti <prototype3628800@yandex.ru>
2022-02-19 20:22:51 +0300

    add school21 exam-05

---
+++ School-21/exam-05/cpp-00/Warlock.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Warlock.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlucanti <tlucanti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/02 12:40:23 by tlucanti          #+#    #+#             */
+/*   Updated: 2022/02/02 13:06:56 by tlucanti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+
+using namespace::std;
+
+class Warlock
+{
+public:
+	Warlock(const string &_n, const string &_t) : name(_n), title(_t)
+	{ cout << _n << ": This looks like another boring day.\n"; }
+	~Warlock() { cout << name << ": My job here is done!\n"; }
+	const string &getName() const { return name; }
+	const string &getTitle() const { return title; }
+	void setTitle(const string &_t) { title = _t; }
+	void introduce() const { cout << name+": I am "+name+", "+title+"!\n"; }
+
+private:
+	string name;
+	string title;
+
+	Warlock(const Warlock &_w);
+	Warlock();
+	Warlock &operator =(const Warlock &_w);
+};
---
+++ School-21/exam-05/cpp-00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlucanti <tlucanti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/02 12:41:01 by tlucanti          #+#    #+#             */
+/*   Updated: 2022/02/02 13:02:33 by tlucanti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Warlock.hpp"
+
+int main()
+{
+  Warlock const richard("Richard", "Mistress of Magma");
+  richard.introduce();
+  std::cout << richard.getName() << " - " << richard.getTitle() << std::endl;
+
+  Warlock* jack = new Warlock("Jack", "the Long");
+  jack->introduce();
+  jack->setTitle("the Mighty");
+  jack->introduce();
+
+  delete jack;
+
+  return (0);
+}
