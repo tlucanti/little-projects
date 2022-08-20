bdfb137eadea954036af0d9b9dec7597112d1d39
tlucanti <prototype3628800@yandex.ru>
2022-08-20 14:21:45 +0300

    fixed miniserv

--- School-21/exam-06/miniserv.c
+++ School-21/exam-06/miniserv.c
@@ -9,16 +9,17 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-void pt(char *s) {
-	int i=0;
-	for(;s[i];++i);
-	write(2,s,i);
+int MSG_SIZE = 262143;
+
+void pt(char *s) { write(2,s,strlen(s)); }
+
+void ft_memcpy(char *dst, char *src, long size) {
+	for (long i=0; i < size; ++i) dst[i] = src[i];
 }
 
 int fatal() {
 	pt("fatal\n");
 	exit(1);
-	return 0;
 }
 
 typedef struct s_list {
@@ -29,7 +30,8 @@ typedef struct s_list {
 
 void *xmalloc(long size) {
 	void *ret = memset(malloc(size), 0, size);
-	return ret ? ret : (void *)(size_t)fatal();
+	if (ret == NULL) fatal();
+	return ret;
 }
 
 void client_add(t_list **the_list, int fd, int id) {
@@ -45,25 +47,24 @@ void client_add(t_list **the_list, int fd, int id) {
 	root->next = node;
 }
 
-long do_send(int fd, char *msg, long size) {
+void do_send(int fd, char *msg, long size) {
 	long c, sent = 0;
 	while (sent < size) {
-		if ((c = send(fd, msg + sent, size - sent, 0)) < 0)
-			return -1;
+		c = send(fd, msg + sent, size - sent, 0);
+		if (c < 0)
+			fatal();
 		sent += c;
 	}
-	return sent;
 }
 
 long do_recv(int fd, char *dst) {
 	long c, rc = 0;
-	do {
+	while (1) {
 		c = recv(fd, dst, 65535, 0);
-		if (c < 0)
-			break;
+		if (c <= 0)
+			return rc;
 		rc += c;
-	} while (c);
-	return rc;
+	}
 }
 
 void remove_client(t_list **the_root, int fd) {
@@ -81,18 +82,20 @@ void remove_client(t_list **the_root, int fd) {
 	root->next = next;
 }
 
-void send_all(t_list **the_root, char *msg) {
+void send_all_except(t_list **the_root, char *msg, int fd) {
 	t_list *root = *the_root;
 	while (root) {
-		if (do_send(root->fd, msg, strlen(msg)) < 0)
-			fatal();
+		if (root->fd != fd)
+			do_send(root->fd, msg, strlen(msg));
 		root = root->next;
 	}
 }
 
 void parse_msg(char msg[], int id) {
-	static char buf[65535];
-	bzero(buf, 65535);
+	static char *buf = NULL;
+	if (buf == NULL)
+		buf = xmalloc(MSG_SIZE);
+	bzero(buf, MSG_SIZE);
 	int i=0;
 	int pos = sprintf(buf, "client %d: ", id);
 	while (msg[i]) {
@@ -103,7 +106,7 @@ void parse_msg(char msg[], int id) {
 		++i;
 	}
 	buf[pos] = '\n';
-	memcpy(msg, buf, 65535);
+	ft_memcpy(msg, buf, pos + 1);
 }
 
 int main(int argc, char **argv) {
@@ -113,7 +116,6 @@ int main(int argc, char **argv) {
 		pt("wrong argumenst\n");
 		exit(1);
 	}
-	int port = atoi(argv[1]);
 	sock = socket(AF_INET, SOCK_STREAM, 0); 
 	fcntl(sock, F_SETFL, O_NONBLOCK);
 	if (sock < 0)
@@ -122,18 +124,17 @@ int main(int argc, char **argv) {
 	bzero(&cli_addr, sizeof(cli_addr));
 	sock_addr.sin_family = AF_INET; 
 	sock_addr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	sock_addr.sin_port = htons(port);
-	if ((bind(sock, (const struct sockaddr *)&sock_addr, sizeof(sock_addr))) != 0)
+	sock_addr.sin_port = htons(atoi(argv[1]));
+	if (bind(sock, (const struct sockaddr *)&sock_addr, sizeof(sock_addr)))
 		fatal();
-	if (listen(sock, 10) != 0)
+	if (listen(sock, 10))
 		fatal();
 	fd_set in, out;
-	char msg[65535];
+	char *msg = xmalloc(MSG_SIZE);
 	t_list *clients = NULL;
 	int last_client = 0;
 	int max_fd = sock;
 	FD_ZERO(&out);
-	struct timeval tv = {0, 900000};
 	while (1) {
 		FD_ZERO(&in);
 		FD_SET(sock, &in);
@@ -142,9 +143,9 @@ int main(int argc, char **argv) {
 			FD_SET(root->fd, &in);
 			root = root->next;
 		}
-		//printf("selecting\n");
-		int sel = select(max_fd + 100, &in, &out, NULL, &tv);
-		//printf("selected %d\n", sel);	
+		int sel = select(max_fd + 100, &in, &out, NULL, NULL);
+		if (sel <= 0)
+			continue ;
 		if (FD_ISSET(sock, &in)) {
 			unsigned len = sizeof(cli_addr);
 			client = accept(sock, (struct sockaddr *)&cli_addr, &len);
@@ -154,7 +155,7 @@ int main(int argc, char **argv) {
 			client_add(&clients, client, last_client);
 			char buf[127];
 			sprintf(buf, "new client %d\n", last_client);
-			send_all(&clients, buf);
+			send_all_except(&clients, buf, client);
 			++last_client;
 			continue ;
 		}
@@ -162,21 +163,20 @@ int main(int argc, char **argv) {
 		while (root) {
 			t_list *next = root->next;
 			if (FD_ISSET(root->fd, &in)) {
-				bzero(msg, 65535);
+				bzero(msg, MSG_SIZE);
 				int r = do_recv(root->fd, msg);
 				if (r <= 0)
 				{
 					int id = root->id;
-					//printf("client %d died\n", root->fd);
 					remove_client(&clients, root->fd);
-					char buf[100];
+					char buf[127];
 					sprintf(buf, "server: client %d died\n", id);
-					send_all(&clients, buf);
+					send_all_except(&clients, buf, -1);
 					root = next;
 					continue ;
 				}
 				parse_msg(msg, root->id);
-				send_all(&clients, msg);
+				send_all_except(&clients, msg, root->fd);
 			}
 			root = next;
 		}
