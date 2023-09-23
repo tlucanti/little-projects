ecda44eb9ed7e0e50707fd4fb394407550b57364
tlucanti <prototype3628800@yandex.ru>
2023-09-24 00:40:05 +0300

    started task-1

---
+++ open-scaler/task-1/main.py
@@ -0,0 +1,55 @@
+
+from num import numbers
+import number_parser
+import sys
+
+def hamming(a, b):
+    assert len(a) == len(b)
+
+    res = 0
+    for c1, c2 in zip(a, b):
+        res += c1 != c2
+
+    return res / len(a)
+
+
+def find_nearest(w):
+    closest = None
+    dist = len(w)
+
+    if w in numbers:
+        # print(f'found: {w} = {numbers[w]}')
+        return w
+
+    for num in numbers:
+        if len(num) != len(w):
+            continue
+
+        ham = hamming(num, w)
+        if ham < dist:
+            dist = ham
+            closest = num
+
+    # print(f'found: {w} = {numbers[closest]} (dist={dist})')
+    assert dist <= 1 / len(w)
+    return closest
+
+
+def parse(s):
+    fixed = ' '.join(find_nearest(w) for w in s.split())
+    res = number_parser.parse_number(fixed)
+
+    assert res is not None
+    return res
+
+
+def main():
+    txt = sys.stdin.read()
+
+    numbers = [parse(s) for s in txt.split(',')]
+    print(numbers)
+
+
+if __name__ == '__main__':
+    main()
+
